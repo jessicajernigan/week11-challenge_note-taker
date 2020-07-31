77127db95af14d8c168f741a62b1c7ce6a34b79d
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-07-31 18:50:34 -0500

    added files, started adding js

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { notes } = require('../../data/db.json');
+const createNote = require('../../lib/Note.js')
+
+
+router.get('/notes', (req, res) => {
+  let results = notes;
+  res.json(results);
+});
+
+
+router.post('/notes', (req, res) => {
+
+  let highestId = notes[notes.length - 1].id;
+
+  req.body.id = (parseInt(highestId) + 1).toString();
+
+    const note = createNote(req.body, notes);
+
+    res.json(note);
+;})
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+// Server port reservation variable
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+const express = require('express');
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Parse incoming string or array data.
+app.use(express.urlencoded({ extended: true }));
+
+// Parse incoming JSON data.
+app.use(express.json());
+
+// Enables server to access apiRoutes.
+app.use('/api', apiRoutes);
+
+// Enables server to access htmlRoutes.
+app.use('/', htmlRoutes);
+
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => {
+  console.log(`Your API server is locked and loaded on port ${PORT}!`);
+});
